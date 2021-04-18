ffdb4abc5148c4240c34758bb8b481fc4cad155b
kimhyekyung <kimhy@LAPTOP-SU1E0UPQ>
2021-04-18 14:25:43 +0900

    0418

--- src/main/java/hongkong/utils/WeatherService.java
+++ src/main/java/hongkong/utils/WeatherService.java
@@ -22,7 +22,16 @@ public class WeatherService {
 		    urlBuilder.append("&" + URLEncoder.encode("lang", "UTF-8") + "=kr");
 		    urlBuilder.append("&" + URLEncoder.encode("units", "UTF-8") + "=metric");
 
+		    
+		    //RestTemplate
+		    //간편하게 Rest 방식 api를 호출할 수 있는 spring 내장 클래스
+		    //json , xml응답을 모두 받을 수 있다.
+		    //여기에서는 json형식으로 응답을 받는다!
+		    //리턴퇴는 데이터를 가져오기 위해서 RestTemplate객체를 이용했다
 		    RestTemplate restTemplate = new RestTemplate();
+		    
+		    //getForObject: 주어진 URL 주소로 HTTP GET 메서드로 객체로 결과를 반환받는다
+		    //getForObject를 통해 클래스 구조에 자동 매핑
 		    OpenWeather response = restTemplate.getForObject(urlBuilder.toString(), OpenWeather.class);
 
 		    return response;
